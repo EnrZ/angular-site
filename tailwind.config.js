e4d13184fffeb29a26e7408adc25f24262ec3502
EnrZ <ez_25_25@yahoo.com>
2023-05-23 01:12:29 -0500

    responsive section with header, image, paragraph started

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    screens:{
+     sm: '480px',
+     md: '768px',
+     lg: '976px',
+     xl: '1440px' 
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+      }
+    },
   },
   plugins: [],
 }
